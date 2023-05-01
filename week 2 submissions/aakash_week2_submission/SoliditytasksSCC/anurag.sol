6a2bbe2a18847b8ad5b2650e209bf23e161701eb
anuragrege <anuragrege@gmail.com>
2023-05-01 22:58:52 +0530

    Revert "Add files via upload"
    This reverts commit 490f419225249d1ebba951d85d1bdec7d25cde56.

--- week 2 submissions/aakash_week2_submission/SoliditytasksSCC/anurag.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.8.0;// SPDX-License-Identifier: MIT
-
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
-
-contract anurag is ERC20 {
-    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
-        _mint(msg.sender, 20 * 10 ** 18);
-    }
-}
